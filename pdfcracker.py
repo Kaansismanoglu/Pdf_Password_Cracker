6d8d824587f8e2609b8a824d56cc223ee80b9cd9
Kaan Şişmanoğlu <125768363+Kaansismanoglu@users.noreply.github.com>
2023-05-27 13:15:10 +0300

    pdfcracker.py

--- pdfcracker.py
+++ pdfcracker.py
@@ -6,19 +6,19 @@ def open_encrypted_pdf(file_path, password):
             pdf_reader = PyPDF2.PdfReader(file)
             if pdf_reader.is_encrypted:
                 if pdf_reader.decrypt(password) == 1:
-                    print("PDF başarıyla açıldı. Şifre:", password)
+                    print("PDF is open successfully. Your Password:", password)
                     return True
                 else:
-                    print("Hatalı şifre:", password)
+                    print("Incorrect Password:", password)
             else:
-                print("PDF şifreli değil.")
+                print("PDF hasn't password.")
     except Exception as e:
-        print("PDF açma hatası:", e)
+        print("PDF opening error:", e)
     return False
 
 start_num = 400000
 end_num = 700000
-pdf_file = "/Users/kaansismanoglu/Downloads/Key_akmerici.pdf"
+pdf_file = "Your File"
 
 for num in range(start_num, end_num+1):
     password = str(num)
